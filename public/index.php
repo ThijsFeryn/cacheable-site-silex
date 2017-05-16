9c4bdfa1e46315ac91d8fe72294a6ff5b8b881b1
Thijs Feryn <thijs@combellgroup.com>
2017-05-16 16:28:40 +0200

    Step 4

--- public/index.php
+++ public/index.php
@@ -6,8 +6,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\HttpFoundation\Cookie;
 use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\HttpCacheServiceProvider;
+use Firebase\JWT\JWT;
 
 if (php_sapi_name() == 'cli-server' && preg_match('/\.(?:png|jpg|jpeg|gif|css|js|ico|ttf|woff|json|html|htm)$/', $_SERVER["REQUEST_URI"])) {
     return false;
@@ -16,6 +18,7 @@ if (php_sapi_name() == 'cli-server' && preg_match('/\.(?:png|jpg|jpeg|gif|css|js
 $app = new Silex\Application();
 $app['locale'] = 'en';
 $app['debug'] = true;
+$app['jwtKey'] = 'SlowWebSitesSuck';
 $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__.'/../views']);
 $app->register(new Silex\Provider\TranslationServiceProvider(), ['locale_fallbacks' => ['en','nl']]);
 $app->register(new Silex\Provider\SessionServiceProvider());
@@ -33,9 +36,40 @@ $app['credentials'] = [
     'admin' => '$2y$10$431rvq1qS9ewNFP0Gti/o.kBbuMK4zs8IDTLlxm5uzV7cbv8wKt0K'
 ];
 
+$app['jwtEncode'] = function() use ($app){
+    return function($username) use ($app) {
+        return JWT::encode([
+            'sub'=>$username,
+            'login'=>true,
+        ],$app['jwtKey']);
+    };
+};
+
+$app['jwtValidate'] = function() use ($app) {
+    return function($token) use ($app) {
+        try {
+            $data = JWT::decode($token,$app['jwtKey'],['HS256']);
+            $data = (array)$data;
+            if(!isset($app['credentials'][$data['sub']])) {
+                return false;
+            }
+            return true;
+        } catch(UnexpectedValueException $e) {
+            return false;
+        }
+    };
+};
+
 $app->before(function (Request $request) use ($app){
     $request->setLocale($request->getPreferredLanguage());
     $app['translator']->setLocale($request->getPreferredLanguage());
+    if($request->getRequestUri() == $app['url_generator']->generate('private')
+        && !$app['jwtValidate']($request->cookies->get('token'))) {
+        $response = new RedirectResponse($app['url_generator']->generate('login'));
+        $response
+            ->setPublic()
+            ->setSharedMaxAge(500);
+    }
 });
 
 $app->after(function(Request $request, Response $response) use ($app){
@@ -58,6 +92,7 @@ $app->get('/header', function () use($app) {
     $response =  new Response($app['twig']->render('header.twig'),200);
     $response
         ->setSharedMaxAge(500)
+        ->setVary('X-Login',false)
         ->setPublic();
     return $response;
 })->bind('header');
@@ -69,7 +104,7 @@ $app->get('/footer', function () use($app) {
     return $response;
 })->bind('footer');
 $app->get('/nav', function (Request $request) use($app) {
-    if($app['session']->has('username')) {
+    if($app['jwtValidate']($request->cookies->get('token'))) {
         $loginLogoutUrl = $app['url_generator']->generate('logout');
         $loginLogoutLabel = 'log_out';
     } else {
@@ -81,13 +116,15 @@ $app->get('/nav', function (Request $request) use($app) {
     $response->headers->addCacheControlDirective('no-cache', true);
     $response
         ->setSharedMaxAge(0)
-        ->setPrivate();
+        ->setVary('X-Login',false)
+        ->setSharedMaxAge(500)
+        ->setPublic();
     return $response;
 })->bind('nav');
 
 
 $app->get('/login', function (Request $request) use($app) {
-    if($app['session']->has('username')) {
+    if($app['jwtValidate']($request->cookies->get('token'))) {
         return new RedirectResponse($app['url_generator']->generate('home'));
     }
     $loginLogoutUrl = $app['url_generator']->generate('login');
@@ -95,13 +132,14 @@ $app->get('/login', function (Request $request) use($app) {
     $response =  new Response($app['twig']->render('login.twig',['loginLogoutUrl'=>$loginLogoutUrl,'loginLogoutLabel'=>$loginLogoutLabel]),200);
     $response
         ->setSharedMaxAge(500)
+        ->setVary('X-Login',false)
         ->setPublic();
     return $response;
 })->bind('login');
 
 $app->get('/logout', function () use($app) {
     $response = new RedirectResponse($app['url_generator']->generate('login'));
-    $app['session']->invalidate();
+    $response->headers->clearCookie('token');
     return $response;
 })->bind('logout');
 
@@ -113,8 +151,9 @@ $app->post('/login', function (Request $request) use($app) {
         return new RedirectResponse($app['url_generator']->generate('login'));
     }
 
-    $app['session']->set('username',$username);
+    $cookie = new Cookie("token",$app['jwtEncode']($username), time() + (3600 * 48), '/', null, false, false);
     $response = new RedirectResponse($app['url_generator']->generate('home'));
+    $response->headers->setCookie($cookie);
     return $response;
 })->bind('loginpost');
 
@@ -123,11 +162,9 @@ $app->get('/private', function () use($app) {
         return new RedirectResponse($app['url_generator']->generate('login'));
     }
     $response =  new Response($app['twig']->render('private.twig'),200);
-    $response->headers->addCacheControlDirective('no-store', true);
-    $response->headers->addCacheControlDirective('no-cache', true);
     $response
-        ->setSharedMaxAge(0)
-        ->setPrivate();
+        ->setSharedMaxAge(500)
+        ->setPublic();
     return $response;
 })->bind('private');
 
--- vcl/default.vcl
+++ vcl/default.vcl
@@ -1,5 +1,6 @@
 vcl 4.0;
 
+import digest;
 import std;
 
 backend default {
@@ -9,26 +10,84 @@ backend default {
 
 sub vcl_recv {
     set req.url = std.querysort(req.url);
-
     if(req.http.accept-language ~ "^\s*(nl)") {
         set req.http.accept-language = regsub(req.http.accept-language,"^\s*(nl).*$","\1");
     } else {
         set req.http.accept-language = "en";
     }
-
     set req.http.Surrogate-Capability="key=ESI/1.0";
-
     if ((req.method != "GET" && req.method != "HEAD") || req.http.Authorization) {
         return (pass);
     }
-
+    call jwt;
+    if(req.url == "/private" && req.http.X-Login != "true") {
+        return(synth(302,"/login"));
+    }
     return(hash);
 }
 
 sub vcl_backend_response {
+    set beresp.http.x-host = bereq.http.host;
+    set beresp.http.x-url = bereq.url;
     if(beresp.http.Surrogate-Control~"ESI/1.0") {
         unset beresp.http.Surrogate-Control;
         set beresp.do_esi=true;
         return(deliver);
+    }
+}
+
+sub vcl_deliver {
+    unset resp.http.x-host;
+    unset resp.http.x-url;
+    unset resp.http.vary;
+}
+
+sub vcl_synth {
+    if (resp.status == 301 || resp.status == 302) {
+        set resp.http.location = resp.reason;
+        set resp.reason = "Moved";
+        return (deliver);
+    }
+}
+
+sub jwt {
+    if(req.http.cookie ~ "^([^;]+;[ ]*)*token=[^\.]+\.[^\.]+\.[^\.]+([ ]*;[^;]+)*$") {
+        set req.http.x-token = ";" + req.http.Cookie;
+        set req.http.x-token = regsuball(req.http.x-token, "; +", ";");
+        set req.http.x-token = regsuball(req.http.x-token, ";(token)=","; \1=");
+        set req.http.x-token = regsuball(req.http.x-token, ";[^ ][^;]*", "");
+        set req.http.x-token = regsuball(req.http.x-token, "^[; ]+|[; ]+$", "");
+
+        set req.http.tmpHeader = regsub(req.http.x-token,"token=([^\.]+)\.[^\.]+\.[^\.]+","\1");
+        set req.http.tmpTyp = regsub(digest.base64url_decode(req.http.tmpHeader),{"^.*?"typ"\s*:\s*"(\w+)".*?$"},"\1");
+        set req.http.tmpAlg = regsub(digest.base64url_decode(req.http.tmpHeader),{"^.*?"alg"\s*:\s*"(\w+)".*?$"},"\1");
+
+        if(req.http.tmpTyp != "JWT") {
+            return(synth(400, "Token is not a JWT: " + req.http.tmpHeader));
+        }
+        if(req.http.tmpAlg != "HS256") {
+            return(synth(400, "Token does not use HS256 hashing"));
+        }
+
+        set req.http.tmpPayload = regsub(req.http.x-token,"token=[^\.]+\.([^\.]+)\.[^\.]+$","\1");
+
+        set req.http.tmpRequestSig = regsub(req.http.x-token,"^[^\.]+\.[^\.]+\.([^\.]+)$","\1");
+        set req.http.tmpCorrectSig = digest.base64url_nopad_hex(digest.hmac_sha256("SlowWebSitesSuck",req.http.tmpHeader + "." + req.http.tmpPayload));
+
+        if(req.http.tmpRequestSig != req.http.tmpCorrectSig) {
+            return(synth(403, "Invalid JWT signature"));
+        }
+
+        set req.http.tmpPayload = digest.base64url_decode(req.http.tmpPayload);
+        set req.http.X-Login = regsub(req.http.tmpPayload,{"^.*?"login"\s*:\s*(\w+).*?$"},"\1");
+        set req.http.X-Username = regsub(req.http.tmpPayload,{"^.*?"sub"\s*:\s*"(\w+)".*?$"},"\1");
+
+        unset req.http.tmpHeader;
+        unset req.http.tmpTyp;
+        unset req.http.tmpAlg;
+        unset req.http.tmpPayload;
+        unset req.http.tmpRequestSig;
+        unset req.http.tmpCorrectSig;
+        unset req.http.tmpPayload;
     }
 }
