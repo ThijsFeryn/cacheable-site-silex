24de0206a286c25afafb3d5125d2c1797e81df05
Thijs Feryn <thijs@combellgroup.com>
2017-05-16 14:48:29 +0200

    Step 1

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
 
 if (php_sapi_name() == 'cli-server' && preg_match('/\.(?:png|jpg|jpeg|gif|css|js|ico|ttf|woff|json|html|htm)$/', $_SERVER["REQUEST_URI"])) {
     return false;
@@ -18,36 +18,12 @@ $app->register(new Silex\Provider\TwigServiceProvider(), ['twig.path' => __DIR__
 $app->register(new Silex\Provider\TranslationServiceProvider(), ['locale_fallbacks' => ['en','nl']]);
 $app->register(new Silex\Provider\SessionServiceProvider());
 
-$app['translator.domains'] = [
-    'messages' => [
-        'en' => [
-            'home'        => 'Home',
-            'welcome'     => 'Welcome to the site',
-            'rendered'    => 'Rendered at %date%',
-            'example'     => 'An example page',
-            'log_in'      => 'Log in',
-            'login'       => 'Login',
-            'log_out'     => 'Log out',
-            'username'    => 'Username',
-            'password'    => 'Password',
-            'private'     => 'Private',
-            'privatetext' => 'Looks like some very private data',
-        ],
-        'nl' => [
-            'home'        => 'Start',
-            'welcome'     => 'Welkom op de site',
-            'rendered'    => 'Samengesteld op %date%',
-            'example'     => 'Een voorbeeldpagina',
-            'log_in'      => 'Inloggen',
-            'login'       => 'Login',
-            'log_out'     => 'Uitloggen',
-            'username'    => 'Gebruikersnaam',
-            'password'    => 'Wachtwoord',
-            'private'     => 'Privé',
-            'privatetext' => 'Deze tekst ziet er vrij privé uit',
-        ]
-    ]
-];
+$app->extend('translator', function($translator, $app) {
+    $translator->addLoader('yaml', new YamlFileLoader());
+    $translator->addResource('yaml', dirname(__DIR__).'/locales/en.yml', 'en');
+    $translator->addResource('yaml',dirname( __DIR__).'/locales/nl.yml', 'nl');
+    return $translator;
+});
 
 $app['credentials'] = [
     'admin' => '$2y$10$431rvq1qS9ewNFP0Gti/o.kBbuMK4zs8IDTLlxm5uzV7cbv8wKt0K'
@@ -56,10 +32,6 @@ $app['credentials'] = [
 $app->before(function (Request $request) use ($app){
     $request->setLocale($request->getPreferredLanguage());
     $app['translator']->setLocale($request->getPreferredLanguage());
-    if($request->getRequestUri() == $app['url_generator']->generate('private')) {
-        $response = new RedirectResponse($app['url_generator']->generate('login'));
-        return $response;
-    }
 });
 
 $app->after(function(Request $request, Response $response) use ($app){
